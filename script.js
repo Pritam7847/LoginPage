b4629b50f5bffc7c8a0fc1421a94509ef6f47226
PRITAM ANAND <himanshusingh7847@gmail.com>
2024-10-27 16:30:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('loginButton').addEventListener('click', function () {
+    alert('Login button clicked!');
+});
+
+document.querySelector('.signup').addEventListener('click', function () {
+    alert('Redirecting to Sign Up page!');
+});
